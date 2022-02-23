03b19f1651bc1155310a1f49bf31f15fa961de60
Artem <artemfr@hotmail.com>
2022-02-23 15:40:51 +0100

    [Last one]
    Last push with all changes

--- Projet_formulaire_07.02/Javascript/script.js
+++ Projet_formulaire_07.02/Javascript/script.js
@@ -1,45 +1,100 @@
-  $(document).ready(function () {
-    $("form").submit(function (event) {
-        var formData = {
-            Name: $("#Name").val(),
-            Email: $("#Email").val(),
-            subject: $("#Subject").val(),
-            Message: $("#Message").val(),
-        };
-        
-        $.ajax({
-            type: "POST",
-            url: "Php.php",
-            data: formData,
-            dataType: "json",
-            encode: true,
-        }).done(function (data) {
-            console.log(data);
-        });
-        
-        event.preventDefault();
-    });
-});
-function submitForm(e, form){
-  e.preventDefault();
-  
-  fetch('index.php', {
-    method: 'post',
-    body: JSON.stringify({name: form.Name.value, email: form.Email.value, subject: form.Subject.value, message: form.Message.value})
-  }).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    //Success code goes here
-    alert('form submited')
-  }).catch(function(err) {
-    //Failure
-    alert('Error')
-  });
+let bye = document.getElementById('adios')
+let toto = document.querySelector('#spanResult')
+let tata = document.querySelector('#spanResult2')
+let tutu = document.querySelector('#spanResult3')
+let titi = document.querySelector('#spanResult4')
+let cotelette = document.querySelector('main')
+const form = document.querySelector('form');
+res = document.getElementById('orange')
+form.addEventListener('submit', function(e) {
+e.preventDefault()
+let fields = {}
+fields.text1 = document.querySelector('.text1').value;
+fields.text2 = document.querySelector('.text2').value;
+fields.text3 = document.querySelector('.text3').value;
+fields.text4 = document.querySelector('.text4').value;
+if (fields.text1 == "") {
+    toto.innerHTML = 'Mettez les données requises'
+    toto.style.display = 'flex'
 }
-
-function send(e,form) {
-  fetch(form.action, {method:'post', body: new FormData(form)});
-
-  console.log('We send post asynchronously (AJAX)');
-  e.preventDefault();
+if (fields.text2 == "") {
+    tata.innerHTML = 'Mettez les données requises'
+    tata.style.display = 'flex'
 }
+if (fields.text3 == "") {
+    tutu.innerHTML = 'Mettez les données requises'
+    tutu.style.display = 'flex'
+}
+if (fields.text4 == "") {
+    titi.innerHTML = 'Mettez les données requises'
+    titi.style.display = 'flex'
+}
+function isEmail(text2) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(text2).toLowerCase());
+   }
+function isName(text1){
+    let regex1 = /^[A-Za-z][A-Za-z]+$/;
+    return regex1.test(text1)
+}   
+function isSubject(text3){
+    let regex2 = /^[A-Za-z][A-Za-z]+$/;
+    return regex2.test(text3);
+}
+function isMessage(text4){
+    let regex3 = /^[A-Za-z][A-Za-z]+$/;
+    return regex3.test(text4);
+}
+console.log(isEmail(fields.text2));
+console.log(isName(fields.text1));
+console.log(isSubject(fields.text3));
+console.log(isMessage(fields.text4));
+if (isNotEmpty(fields.text1) && isNotEmpty(fields.text2) && isNotEmpty(fields.text3) && isNotEmpty(fields.text4)) {
+    if (isEmail(fields.text2) && isName(fields.text1) && isSubject(fields.text3) && isMessage(fields.text4)) {
+         const formData = new FormData(form);
+        fetch('./PHP/index.php', {
+        method: 'POST',
+        body: formData
+        })
+        .then((response) => {
+        return response.json()
+        }) 
+        .then((data) => {
+        console.log(data);
+        res.style.display = 'flex'
+        cotelette.classList.add('qqch')
+         
+    })
+}
+else {
+    console.log('toto')
+}
+    }
+    else{
+        console.log('tutu')
+    }
+})
+ /* NOT EMPTY */
+
+   function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+
+   bye.addEventListener('click', function () {
+       console.log('tumtum');
+        form.reset()
+        res.style.display = 'none'
+        cotelette.classList.remove('qqch')
+   }
+/* EMAIL */
+
+
+
+
+//    function restart() {
+//     if (res == true) return res.style.display = 'flex' 
+//    }
+
+
+   )
--- Projet_formulaire_07.02/PHP/index.php
+++ Projet_formulaire_07.02/PHP/index.php
@@ -1,10 +1,31 @@
 <?php
+$regleNomPrenom = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/";
+$regleEmail = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/";
+$regleMessage = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ',;()-]+$/";
 
-if (isset($_POST['submit'])) {
+if((isset($_POST['Name']))&&(preg_match($regleNomPrenom, $_POST['Name']))){
+    $Name = $_POST['Name'];
+}
+if((isset($_POST['Email']))&&(preg_match($regleEmail, $_POST['Email']))){
+    $Email = $_POST['Email'];
+}
+if((isset($_POST['Subject']))&&(preg_match($regleMessage, $_POST['Subject']))){
+    $Subject = $_POST['Subject'];
+}
+if((isset($_POST['Message']))&&(preg_match($regleMessage, $_POST['Message']))){
+    $Message = $_POST['Message'];
+}
+if(($Name)&&($Email)&&($Subject)&&($Message)){
+    $res = array('validation' => "Email envoyé");
+    echo json_encode($res);
+    mail("a.khliebnikov@codeur.online", $Sujet, $Message, "From: $Email");
+}
+/*if (isset($_POST['submit'])) {
     $name = $_POST['Name'];
     $subject = $_POST['Subject'];
     $mailFrom = $_POST['Email'];
     $message = $_POST['Message'];
+    
 
     $mailTo = "a.khliebnikov@codeur.online";
     $headers = "From: ".$mailFrom;
@@ -14,4 +35,3 @@ if (isset($_POST['submit'])) {
     header("Location: index.php?mailsend");
     exit;
 }
-
